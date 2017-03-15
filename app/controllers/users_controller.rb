043ffca7ec5ddf26001f1b95e151a6c79893d076
Dan <dan@test.com>
2017-03-16 07:13:47 +1100

    show users and gravatar for article

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,10 @@ def new
   @user = User.new
 end
 
+def index
+ @users = User.all
+end
+
 def create
   @user  = User.new(user_params)
   if @user.save
@@ -27,6 +31,12 @@ def update
     render 'edit'
   end
 end
+
+def show
+  @user = User.find(params[:id])
+
+end
+
 
 private
 def user_params
